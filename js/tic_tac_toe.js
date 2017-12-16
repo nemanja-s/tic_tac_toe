99692250aef1c2337f5c8cb2cccb0e423597f857
nemanja-s <s.nemanja@protonmail.ch>
2017-12-16 13:18:18 -0800

    second commit

---
+++ js/tic_tac_toe.js
@@ -0,0 +1,5 @@
+var cells = ["", "", "", "", "", "", "", "", "", ];
+
+function refresh() {
+    $("div#playground").html("<table><tr><td></td><td></td><td></td><tr><td></td><td></td><td></td><tr><td></td><td></td><td></td></table>")
+}
